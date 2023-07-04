7c9b6b98424f1393466e4794719fc814d40f4813
jakovid <jacobk.dowdle@gmail.com>
2023-07-04 11:52:47 +0800

    began to fill out country flag switch statement

--- src/components/TeacherDetails.js
+++ src/components/TeacherDetails.js
@@ -1,8 +1,24 @@
 import { useTeachersContext } from "../hooks/useTeachersContext"
 import { BsFillTrash3Fill } from 'react-icons/bs'
+import TW from '../images/tw.png';
+import USA from '../images/usa.png';
+import CN from '../images/cn.png';
 
 export default function TeacherDetails({ teacher }) {
     const { dispatch } = useTeachersContext()
+
+    function countrySwitch(country) {
+        switch(country) {
+          case 'Taiwan':
+            return TW;
+        case 'USA':
+            return USA;
+        case 'Canada':
+            return CN;
+          default:
+            return 'foo';
+        }
+      };
 
     const handleClick = async () => {
         const response = await fetch('/api/teachers/' + teacher._id, {
@@ -20,6 +36,7 @@ export default function TeacherDetails({ teacher }) {
             <img src={teacher.img} className="teacher-img"/>
             <h4>{teacher.name}</h4>
             <p>{teacher.country}</p>
+            <img src={countrySwitch(teacher.country)} />
             <p>{teacher.bio}</p>
             <span onClick={handleClick} ><BsFillTrash3Fill fill="inherit" /></span>
         </div>
