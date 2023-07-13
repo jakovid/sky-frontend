f6f6e770fc672cd8c40822304b4d6fbe4fa5d652
Jacob Dowdle <jacobk.dowdle@gmail.com>
2023-07-13 22:39:22 +0800

    began to switch current components to tailwind

--- src/components/EngNavbar.js
+++ src/components/EngNavbar.js
@@ -1,25 +1,26 @@
 import { Link } from "react-router-dom";
-import "../styles/navbar.css";
 
 export default function EngNavbar() {
     return(
-        <header className="eng header">
-            <div className="logo navbar">
-                SkyEducation
+        <header className="flex flex-row bg-green-950 text-white justify-between items-center h-16">
+            <div className="bg-red-900 px-3 flex w-60 h-full justify-center items-center">
+                <p >SkyEducation</p>
             </div>
-            <div className="navbar container">
-                <Link to="/">
-                    <p>Home</p>
-                </Link>
-                <Link to="/about-us">
-                    <p>About Us</p>
-                </Link>
-                <Link to="/our-classes">
-                    <p>Our Classes</p>
-                </Link>
-                <Link to="/contact-us">
-                    <p>Contact Us</p>
-                </Link>
+            <div className="pe-5">
+                <div className="flex flex-row gap-4">
+                    <Link to="/">
+                        <p>Home</p>
+                    </Link>
+                    <Link to="/about-us">
+                        <p>About Us</p>
+                    </Link>
+                    <Link to="/our-classes">
+                        <p>Our Classes</p>
+                    </Link>
+                    <Link to="/contact-us">
+                        <p>Contact Us</p>
+                    </Link>
+                </div>
             </div>
         </header>
     )
