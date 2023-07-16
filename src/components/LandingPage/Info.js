7d32257952fac58d7c532a76994bf54e2f07d9a3
Jacob Dowdle <jacobk.dowdle@gmail.com>
2023-07-16 23:50:53 +0800

    began to add images

--- src/components/LandingPage/Info.js
+++ src/components/LandingPage/Info.js
@@ -1,7 +1,12 @@
 export default function BuildInfo() {
     return(
-        <div className="bg-red-900 h-96 p-4 flex flex-col items-center">
+        <div className="bg-red-900 h-96 p-4 flex flex-col items-center gap-10">
             <h2 className="text-6xl">Why learn with us?</h2>
+            <div className="flex justify-evenly w-full">
+                <img className="h-48 rounded-full border-4" src="https://cdn.discordapp.com/attachments/989268383751106560/1129712468206288906/jakovid_Students_in_an_English_classroom_in_Taiwan_with_a_dark__f78af349-a0ba-4e35-ad42-11bf7c50354c.png"/>
+                <img className="h-48 rounded-full border-4" src="https://cdn.discordapp.com/attachments/989268383751106560/1129712468206288906/jakovid_Students_in_an_English_classroom_in_Taiwan_with_a_dark__f78af349-a0ba-4e35-ad42-11bf7c50354c.png"/>
+                <img className="h-48 rounded-full border-4" src="https://cdn.discordapp.com/attachments/989268383751106560/1129712468206288906/jakovid_Students_in_an_English_classroom_in_Taiwan_with_a_dark__f78af349-a0ba-4e35-ad42-11bf7c50354c.png"/>
+            </div>
         </div>
     )
 }
