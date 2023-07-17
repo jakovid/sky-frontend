f8c97a91e9c41528fdf695b881c5ecc7f858cef7
Jacob Dowdle <jacobk.dowdle@gmail.com>
2023-07-17 16:55:23 +0800

    nearly completed home page

--- src/pages/english/LandingPageEng.js
+++ src/pages/english/LandingPageEng.js
@@ -1,13 +1,17 @@
 import EngNavbar from "../../components/EngNavbar"
 import BuildHero from "../../components/LandingPage/Hero"
+import BuildWhyLearn from "../../components/LandingPage/WhyLearn"
 import BuildInfo from "../../components/LandingPage/Info"
+import BuildFooter from "../../components/Footer"
 
 export default function LandingPageEng() {
     return(
         <div>
             <EngNavbar />
             <BuildHero />
+            <BuildWhyLearn />
             <BuildInfo />
+            <BuildFooter />
         </div>
     )
 }
