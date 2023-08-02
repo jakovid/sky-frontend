2c991aae3280dda52cabc0394b2aa8f5227a213c
Jacob Dowdle <jacobk.dowdle@gmail.com>
2023-08-02 21:44:50 +0800

    finished responsive tailwindcss for about us, started our team

--- src/components/AboutUs/Keys.js
+++ src/components/AboutUs/Keys.js
@@ -37,21 +37,21 @@ export default function BuildKeys() {
         <div className='flex flex-col gap-2 bg-red-900/90'>
             <div className="w-full  text-center text-6xl p-4 pt-10">{keysTitle}</div>
             
-            <div className=" flex items-center justify-center pb-28">
-                <div className="w-4/6 max-w-6xl h-full bg-red-900 p-10 grid grid-cols-3 gap-8 rounded-xl border-8 border-green-950">
+            <div className=" flex flex-row items-center justify-center lg:pb-28">
+                <div className="lg:w-4/6 flex flex-col max-w-6xl h-full bg-red-900 p-10 lg:grid lg:grid-cols-3 gap-8 lg:rounded-xl lg:border-8 border-green-950">
                     <div className=" gap-3 flex flex-col items-center text-center">
                         <div><FaUserFriends className='text-8xl border-4 p-2 rounded-full border-[#E0D3AF]' /></div>
-                        <h3 className="text-5xl">{keyTitleOne}</h3>
+                        <h3 className="lg:text-3xl xl:text-4xl text-5xl">{keyTitleOne}</h3>
                         <p>{keyTextTwo}</p>
                     </div>
                     <div className="gap-3 flex flex-col items-center text-center">
                         <div><HiOutlineChat className='text-8xl border-4 p-2 rounded-full border-[#E0D3AF]'/></div>
-                        <h3 className="text-5xl">{keyTitleTwo}</h3>
+                        <h3 className="lg:text-3xl xl:text-4xl text-5xl">{keyTitleTwo}</h3>
                         <p>{keyTextTwo}</p>
                     </div>
                     <div className="gap-3 flex flex-col items-center text-center">
                         <div><GiBrickWall className='text-8xl border-4 p-2 rounded-full border-[#E0D3AF]'/></div>
-                        <h3 className="text-5xl">{keyTitleThree}</h3>
+                        <h3 className="text-5xl xl:text-4xl lg:text-3xl">{keyTitleThree}</h3>
                         <p>{keyTextThree}</p>
                     </div>
                 </div>
--- src/components/OurTeam/Hero.js
+++ src/components/OurTeam/Hero.js
@@ -15,17 +15,17 @@ export default function BuildHero() {
 
     return(
         <div className="w-full flex flex-col">
-            <div className="h-96 bg-green-950/90 flex flex-row items-center">
-                <div className="w-2/3 h-full flex justify-center items-center gap-3 text-center bg-center bg-cover bg-[url('https://cdn.discordapp.com/attachments/989268383751106560/1130786300308492389/jakovid_dark_red_and_dark_green_theme_a_group_of_American_Engli_f421b1a1-a73a-48ac-9451-47eaa85437ab.png')]">
+            <div className="sm:h-96 bg-green-950/90 flex flex-col sm:flex-row items-center">
+                <div className="flex h-56 w-full sm:h-full md:w-2/3 md:h-full justify-center items-center gap-3 text-center bg-center bg-cover bg-[url('https://cdn.discordapp.com/attachments/989268383751106560/1130786300308492389/jakovid_dark_red_and_dark_green_theme_a_group_of_American_Engli_f421b1a1-a73a-48ac-9451-47eaa85437ab.png')]">
                     <div className='w-full flex justify-center bg-gray-300/50 pt-4'>
                            <img src={SkyLogo} />
                     </div>
                 </div>
-                <div className="w-1/3 flex flex-col items-center justify-center gap-4">
-                    <div className='text-6xl'>{title}</div>
+                <div className="w-full sm:w-1/3 text-center flex flex-col items-center justify-center gap-4 py-10 md:py-0">
+                    <div className='text-5xl md:text-6xl'>{title}</div>
                         
-                    <div className='flex items-center justify-center'>
-                        <div className='h-64 w-96 bg-cover bg-center border-8 border-red-900 rounded-xl bg-[url("https://cdn.discordapp.com/attachments/989268383751106560/1130786300308492389/jakovid_dark_red_and_dark_green_theme_a_group_of_American_Engli_f421b1a1-a73a-48ac-9451-47eaa85437ab.png")]'></div>
+                    <div className='flex items-center w-full lg:w-5/6 justify-center p-2'>
+                        <div className='h-64 w-full sm:h-32 bg-cover bg-center border-8 border-red-900 rounded-xl bg-[url("https://cdn.discordapp.com/attachments/989268383751106560/1130786300308492389/jakovid_dark_red_and_dark_green_theme_a_group_of_American_Engli_f421b1a1-a73a-48ac-9451-47eaa85437ab.png")]'></div>
                     </div>
                 </div>
             </div>
