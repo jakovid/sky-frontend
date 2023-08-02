bddec447fe38d08bb4c0965ecde7a22d5886dbaa
Jacob Dowdle <jacobk.dowdle@gmail.com>
2023-08-02 16:40:25 +0800

    added mobile friendly tailwindcss to home page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,16 +16,16 @@ export default function BuildFooter(){
     }
 
     return(
-        <footer className='flex flex-row justify-between items-center bg-red-900/90 gap-10 py-3 px-20'>
-            <a href="tel:0225338263" className='text-lg flex gap-2 items-center'><BsTelephone /><div >02 2533 8263</div></a>
+        <footer className='flex flex-row justify-between items-center bg-red-900/90 md:gap-10 py-3 px-20'>
+            <a href="tel:0225338263" className='text-lg flex gap-2 items-center'><BsTelephone /><div className='hidden md:block' >02 2533 8263</div></a>
 
-            <a className='flex gap-2 cursor-pointer items-center' href='https://goo.gl/maps/jDmTTpKEmqy1w6FY9'>
+            <a className='flex gap-2 cursor-pointer text-lg items-center' href='https://goo.gl/maps/jDmTTpKEmqy1w6FY9'>
                 <FaMapPin  />
-                <p >{address}</p>
+                <p className='hidden md:block' >{address}</p>
             </a>
 
             <div className='flex flex-row items-center gap-6 justify-center'>
-                <img className='h-8 bg-[#E0D3AF] pt-1 px-4 rounded-s-full rounded-e-full' src={SkyLogo} />
+                <img className='h-4 md:h-8 bg-[#E0D3AF] pt-1 px-4 rounded-s-full rounded-e-full' src={SkyLogo} />
                 <a href='https://www.facebook.com/Skyeducation2008' target="_blank"><FaFacebook className='text-2xl' /></a>
             </div>
         </footer>
--- src/components/LandingPage/Info.js
+++ src/components/LandingPage/Info.js
@@ -29,26 +29,26 @@ export default function BuildInfo() {
 
     return(
         <div className="bg-green-950/90 flex justify-center">
-            <div className="flex flex-col gap-10 my-10 w-4/6 max-w-6xl">
-                <div className="flex w-full items-center gap-10 bg-green-950 p-5 rounded-xl border-8 border-red-900">
-                    <img className="h-80 rounded-full border-8 border-red-900" src="https://cdn.discordapp.com/attachments/989268383751106560/1130412981453336646/jakovid_Students_being_taught_by_an_American_teacher_in_an_Engl_a8d1ae9e-6764-4d79-8e41-27de5cdf1157.png"/>
+            <div className="flex flex-col gap-10 my-10 xl:w-4/6 xl:max-w-6xl">
+                <div className="flex flex-col lg:flex-row w-full items-center gap-4 lg:gap-10 bg-green-950 p-5 lg:rounded-xl lg:border-8 lg:border-red-900">
+                    <img className="h-72 lg:h-80 rounded-full border-8 border-red-900" src="https://cdn.discordapp.com/attachments/989268383751106560/1130412981453336646/jakovid_Students_being_taught_by_an_American_teacher_in_an_Engl_a8d1ae9e-6764-4d79-8e41-27de5cdf1157.png"/>
                     <div className="flex flex-col w-full items-center text-center gap-6 justify-center">
-                        <h2 className="text-6xl flex flex-wrap leading-tight">{titleOne}</h2>
-                        <p className="text-xl">{textOne}</p>
+                        <h2 className="text-4xl lg:text-6xl flex flex-wrap leading-tight">{titleOne}</h2>
+                        <p className="text-lg lg:text-xl">{textOne}</p>
                     </div>
                 </div>
-                <div className="flex w-full gap-10 bg-green-950 p-5 items-center rounded-xl border-8 border-red-900">
-                    <img className="h-80 rounded-full border-8 border-red-900" src="https://cdn.discordapp.com/attachments/989268383751106560/1130415225502449805/jakovid_Students_raising_their_hands_in_an_English_classroom_in_7ac1f7a2-ad22-4498-b980-26cffadab393.png"/>
+                <div className="flex flex-col lg:flex-row w-full items-center gap-4 lg:gap-10 bg-green-950 p-5 lg:rounded-xl lg:border-8 lg:border-red-900">
+                    <img className="h-72 lg:h-80 rounded-full border-8 border-red-900" src="https://cdn.discordapp.com/attachments/989268383751106560/1130415225502449805/jakovid_Students_raising_their_hands_in_an_English_classroom_in_7ac1f7a2-ad22-4498-b980-26cffadab393.png"/>
                     <div className="flex flex-col w-full items-center text-center gap-6 justify-center">
-                        <h2 className="text-6xl flex flex-wrap leading-tight">{titleTwo}</h2>
-                        <p className="text-xl">{textTwo}</p>
+                        <h2 className="text-4xl lg:text-6xl flex flex-wrap leading-tight">{titleTwo}</h2>
+                        <p className="text-lg lg:text-xl">{textTwo}</p>
                     </div>
                 </div>
-                <div className="flex w-full gap-10 bg-green-950 p-5 items-center rounded-xl border-8 border-red-900">
-                    <img className="h-80 rounded-full border-8 border-red-900" src="https://cdn.discordapp.com/attachments/989268383751106560/1130416420480634931/jakovid_Students_reading_books_in_an_English_classroom_in_Taiwa_ca2b4f01-c6fc-4b42-abea-1e1ec43ff256.png"/>
+                <div className="flex flex-col lg:flex-row w-full items-center gap-4 lg:gap-10 bg-green-950 p-5 lg:rounded-xl lg:border-8 lg:border-red-900">
+                    <img className="h-72 lg:h-80 rounded-full border-8 border-red-900" src="https://cdn.discordapp.com/attachments/989268383751106560/1130416420480634931/jakovid_Students_reading_books_in_an_English_classroom_in_Taiwa_ca2b4f01-c6fc-4b42-abea-1e1ec43ff256.png"/>
                     <div className="flex flex-col w-full items-center gap-6 justify-center text-center">
-                        <h2 className="text-6xl flex flex-wrap leading-tight">{titleThree}</h2>
-                        <p className="text-xl">{textThree}</p>
+                        <h2 className="text-4xl lg:text-6xl flex flex-wrap leading-tight">{titleThree}</h2>
+                        <p className="text-lg lg:text-xl">{textThree}</p>
                     </div>
                 </div>
             </div>
