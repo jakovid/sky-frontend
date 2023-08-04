fac5fb68b694b0bde0acf7023dfeb78e5a4c4170
Jacob Dowdle <jacobk.dowdle@gmail.com>
2023-08-04 17:09:20 +0800

    finished adding content image toggle, and created hook and context for web images

---
+++ src/context/WebImageContext.js
@@ -0,0 +1,41 @@
+import { createContext, useReducer } from "react";
+
+export const WebImagesContext = createContext();
+
+export const webImagesReducer = (state, action) => {
+    switch (action.type) {
+        case 'SET_WEB_IMAGES':
+            return {
+                webImages: action.payload
+            }
+        case 'CREATE_WEB_IMAGE' :
+            return {
+                webImages: [action.payload, ...state.webImages]
+            }
+        case 'DELETE_WEB_IMAGES' :
+            return {
+                webImages: state.webImages.filter((w) => w._id !== action.payload._id )
+            }
+        case 'PATCH_WEB_IMAGES' :
+            return {
+                webImages: state.webImages.map((webImage) =>
+                    webImage._id === action.payload._id ? {...webImage, ...action.payload } : webImage
+                )
+                
+            }
+        default:
+            return state
+    }
+}
+
+export const WebImagesContextProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(webImagesReducer, {
+        webImages: null
+    })
+    
+    return(
+        <WebImagesContext.Provider value={{...state, dispatch}}>
+            { children }
+        </WebImagesContext.Provider >
+    )
+}
--- src/pages/admin/ContactUs.js
+++ src/pages/admin/ContactUs.js
@@ -2,22 +2,35 @@ import AdminNavbar from "../../components/AdminNavbar";
 import { useContentsContext } from "../../hooks/useContentsContext";
 import AdminEditForm from "../../components/AdminComps/AdminEditForm";
 import ContactUsEng from "../public/ContactUsEng";
+import { useState } from "react";
 
 
 export default function AdminContactUs() {
     const { contents } = useContentsContext();
+    const [editType, setEditType] = useState('content')
+
+    const handleClick = (type) => {
+        setEditType(type);
+    }
 
     return(
         <div>
             <AdminNavbar />
             <div className="grid grid-cols-2">
                 <ContactUsEng />
-                
-                <div className="bg-blue-950/90 flex flex-col items-center py-2 gap-2">
-                {contents && contents.slice(58, 65).map((content) => (
-                    <AdminEditForm key={content._id} content={content} />
-                ))}
+
+                <div>
+                    <div className="bg-red-900 p-5 flex items-center justify-center gap-8">
+                        <button onClick={() => {handleClick('content')}} className="bg-green-950 px-8 py-2 rounded-s-full rounded-e-full cursor-pointer ">Content</button>
+                        <button onClick={() => {handleClick('images')}}  className="bg-green-950 px-8 py-2 rounded-s-full rounded-e-full cursor-pointer ">Images</button>
+                    </div>
+                    <div className="bg-blue-950/90 flex flex-col items-center py-2 gap-2">
+                        {editType === 'content' && contents && contents.slice(58, 65).map((content) => (
+                        <AdminEditForm key={content._id} content={content} />
+                    ))}
+                    </div>
                 </div>
+                
             </div>
         </div>
     )
