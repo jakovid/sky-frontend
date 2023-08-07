ad4cdc03b9e66ed47392273c77a9ccb3b383ee80
Jacob Dowdle <jacobk.dowdle@gmail.com>
2023-08-07 21:51:44 +0800

    changed all images to be srced from db

--- src/components/OurClasses/Hero.js
+++ src/components/OurClasses/Hero.js
@@ -1,11 +1,14 @@
 import { useLanguageContext } from '../../hooks/useLanguageContext'
 import { useContentsContext } from '../../hooks/useContentsContext';
+import { useWebImagesContext } from '../../hooks/useWebImagesContext';
 import SkyLogo from '../../images/SkyLogo2.webp';
 
 export default function BuildHero() {
     const { language } = useLanguageContext();
     const { contents } = useContentsContext();
+    const { webImages } = useWebImagesContext();
 
+    // declare content variables
     let title = null;
 
     if (contents) {
@@ -13,10 +16,21 @@ export default function BuildHero() {
         if (ourClassesTitle) { title = ourClassesTitle[language]}
     }
 
+    // declare web image variables
+    let heroMainImage = null, heroSubImage = null;
+
+    if (webImages) {
+        const heroMain = webImages.find(webImage => webImage.name === "Our Classes Hero Main");
+        if (heroMain) { heroMainImage = heroMain.img_url };
+
+        const heroSub = webImages.find(webImage => webImage.name === "Our Classes Hero Sub");
+        if (heroSub) { heroSubImage = heroSub.img_url };
+    }
+
     return(
         <div className="w-full flex flex-col">
             <div className="sm:h-96 bg-green-950/90 flex flex-col sm:flex-row items-center">
-                <div className="flex h-56 w-full sm:h-full md:w-2/3 justify-center items-center gap-3 text-center bg-center bg-cover bg-[url('https://cdn.discordapp.com/attachments/989268383751106560/1131857448806907934/jakovid_a_dark_red_and_dark_green_themed_English_classroom_with_f18fc469-1906-4c0c-8af7-b055fb3dc00c.png')]">
+                <div className="flex h-56 w-full sm:h-full md:w-2/3 justify-center items-center gap-3 text-center bg-center bg-cover" style={{ backgroundImage: `url(${heroMainImage})` }}>
                     <div className='w-full flex justify-center bg-gray-300/50 pt-4'>
                            <img src={SkyLogo} />
                     </div>
@@ -25,7 +39,7 @@ export default function BuildHero() {
                     <div className='text-5xl md:text-6xl'>{title}</div>
                         
                     <div className='flex items-center w-full lg:w-5/6 justify-center p-2'>
-                        <div className='h-64 w-full sm:h-32 md:h-48 lg:h-52 xl:h-64 bg-cover bg-center border-8 border-red-900 rounded-xl bg-[url("https://cdn.discordapp.com/attachments/989268383751106560/1131858031517372528/jakovid_a_dark_red_and_dark_green_themed_English_classroom_with_5f8d6ec4-549a-4da8-8d65-982efeb85aa0.png")]'></div>
+                        <div className='h-64 w-full sm:h-32 md:h-48 lg:h-52 xl:h-64 bg-cover bg-center border-8 border-red-900 rounded-xl' style={{ backgroundImage: `url(${heroSubImage})` }}></div>
                     </div>
                 </div>
             </div>
